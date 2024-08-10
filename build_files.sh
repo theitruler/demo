673cc9cc77c8f8a9aff0ef63ca9a7574e0bf2893
theitruler <ashishroshanraja@gmail.com>
2024-08-10 16:25:51 +0530

    #last and final try

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+echo "Installing dependencies..."
+python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+echo "Collecting static files..."
 python3 manage.py collectstatic --noinput --clear
+
+echo "Running migrations..."
 python3 manage.py migrate
+
+echo "Build completed."
